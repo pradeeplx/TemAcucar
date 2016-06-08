d294d7428512966ce7e700bee9f22220fcea0e40
Daniel Weinmann <danielweinmann@gmail.com>
2016-06-08 00:43:43 -0300

    Fixes UserMap to use LogoMarker

--- app/components/UserMap.js
+++ app/components/UserMap.js
@@ -1,35 +1,22 @@
 import React, { View, Image, PixelRatio, Dimensions } from 'react-native'
 import MapView from 'react-native-maps'
 import Colors from "../Colors"
+import LogoMarker from "./LogoMarker"
 
 export default UserMap = ({ latitude, longitude, delta, text }) => {
-  const ratio = PixelRatio.get()
-  let image
-  if (ratio <= 1) {
-    image = require('../img/marker100.png')
-  } else if (ratio <= 1.5) {
-    image = require('../img/marker150.png')
-  } else if (ratio <= 2) {
-    image = require('../img/marker200.png')
-  } else if (ratio <= 3) {
-    image = require('../img/marker300.png')
-  } else if (ratio <= 3.5) {
-    image = require('../img/marker350.png')
-  } else {
-    image = require('../img/marker400.png')
-  }
-  const height = Dimensions.get('window').height
-  const defaultDelta = (height < 570 ? 0.0025 : 0.005)
+  const windowHeight = Dimensions.get('window').height
+  const height = windowHeight * (windowHeight < 570 ? 0.12 : 0.20)
+  const defaultDelta = (windowHeight < 570 ? 0.0025 : 0.005)
   return (
     <View>
       <MapView
         showsUserLocation={false}
         zoomEnabled={false}
         rotateEnabled={false}
-        scrollEnabled={true}
+        scrollEnabled={false}
         pitchEnabled={false}
         style={{
-          height: height * (height < 570 ? 0.12 : 0.20),
+          height: height,
           alignSelf: 'stretch',
         }}
         region={{
@@ -38,9 +25,16 @@ export default UserMap = ({ latitude, longitude, delta, text }) => {
           latitudeDelta: parseFloat(delta ? delta : (latitude ? defaultDelta : 50)),
           longitudeDelta: parseFloat(delta ? delta : (longitude ? defaultDelta : 50)),
         }}
-      >
-        { latitude && longitude && <MapView.Marker coordinate={{latitude, longitude}} image={image} /> }
-      </MapView>
+      />
+      <View style={{
+        position: 'absolute',
+        bottom: (height / 2) - 7.5,
+        left: 0,
+        right: 0,
+        alignItems: 'center',
+      }}>
+        <LogoMarker />
+      </View>
       <View style={{
         position: 'absolute',
         bottom: 10,
