63ae3b93daab6b30ec7c319cd9ee92a8a7d77e1c
Daniel Weinmann <danielweinmann@gmail.com>
2016-07-12 02:27:44 -0300

    New round of design

--- app/components/MapText.js
+++ app/components/MapText.js
@@ -1,20 +1,41 @@
-import React, { View, Image, Dimensions } from 'react-native'
+import React, { Component, View } from 'react-native'
 import Colors from "../Colors"
 
-export default MapText = ({ children }) => (
-  <View style={{
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(0,0,0,0.35)',
-  }}>
-    <Sentence style={{
-      color: Colors.white, 
-      textAlign: 'center',
-      paddingVertical: 3,
-    }}>
-      { children }
-    </Sentence>
-  </View>
-)
+export default class MapText extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = { viewHeight: 0 }
+  }
+
+  handleLayout(event) {
+    this.setState({ viewHeight: event.nativeEvent.layout.height })
+  }
+
+  render() {
+    const { children } = this.props
+    const { viewHeight } = this.state
+    return (
+      <View
+        onLayout={this.handleLayout.bind(this)}
+        style={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: 'rgba(0,0,0,0.4)',
+        }}
+      >
+        <Sentence style={{
+          fontSize: 14,
+          color: Colors.white, 
+          textAlign: 'center',
+          paddingVertical: 3,
+          marginTop: viewHeight - 24,
+        }}>
+          { children }
+        </Sentence>
+      </View>
+    )
+  }
+}
