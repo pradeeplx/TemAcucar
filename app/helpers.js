79e69132965ad4395e86ad9876d31f478c6661d8
Daniel Weinmann <danielweinmann@gmail.com>
2016-08-02 13:30:13 -0300

    Updates maximum fontFactor to 1.5

--- app/helpers.js
+++ app/helpers.js
@@ -4,5 +4,6 @@ export function fontFactor() {
   const dimensions = Dimensions.get('window')
   const height = dimensions.height * dimensions.scale
   const factor = (height > 1024 ? ((height / 1024)) : 1)
-  return (factor > 1 ? (factor > 1.4 ? 1.4 : factor) : 1)
+  const maxFactor = 1.5
+  return (factor > 1 ? (factor > maxFactor ? maxFactor : factor) : 1)
 }
