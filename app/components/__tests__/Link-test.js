bf16aabedd4a979e973fef4ccf0880b10f355074
Gustavo Guichard <gustavoguichard@gmail.com>
2016-12-21 18:53:16 -0200

    Update some snapshots

--- app/components/__tests__/Link-test.js
+++ app/components/__tests__/Link-test.js
@@ -5,11 +5,7 @@ import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Link
-      style={{top: 0}}
-    >
-      FooBar
-    </Link>
+    <Link style={{top: 0}}>FooBar</Link>
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
