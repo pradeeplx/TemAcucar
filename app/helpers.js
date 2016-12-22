5d241e0b30f7bf39f07c56baa30266f64ed2bc2e
Gustavo Guichard <gustavoguichard@gmail.com>
2016-12-22 13:09:22 -0200

    Remove useless code

--- app/helpers.js
+++ app/helpers.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Dimensions } from 'react-native'
-import { clamp, last, reduceRight } from 'lodash'
+import { clamp, reduceRight } from 'lodash'
 
 export const calculateFontFactor = ({ height, scale }) => {
   return clamp((height * scale) / 1024, 1, 1.5)
@@ -10,5 +10,5 @@ export const fontFactor = () => calculateFontFactor(Dimensions.get('window'))
 
 export const maxLower = (number, ...options) => {
   return reduceRight(options, (acc, curr) =>
-    number <= curr ? curr : acc, last(options))
+    number <= curr ? curr : acc)
 }
